1782971c2cf12b52c5445b768522ef8d70936e46
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-04-05 21:58:45 +0530

    basic node

---
+++ Inbuilt.js
@@ -0,0 +1,8 @@
+//! INBUILT MODULES
+
+// OS Inbuilt module
+
+const OS = require("os");
+console.log(OS.cpus()[0]);
+
+console.log(OS.version());
--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const { sum, sub, mul, div } = require("./app");
+/* 
+Node Basics :- 
+
+Node Js helps JavaScript to run outside the browser(in browser Js engine was used to interpret js in browser). 
+
+
+Created by Ryan Dahl in 2008
+
+
+Advantage --> It's based on Js hence no need to learn any other language
+
+Note ---> It's a runtime environment but not a programming language used to write backend code.
+
+its --> All functionality of Js Engine + added features (Modules)
+
+import and export are keywords to import and export modules which were given by ES6.
+
+before that common JS module was used
+
+//* Types of Modules in node --> 1.Inbuilt Module:-   inbuilt module->Already installed
+
+//* 2. External modules--> we can get externally
+
+//* 3. User developed modules or Third Party modules    --> we create it / users create --> self created modules
+
+
+
+*/
+const { sum, sub, mul, div } = require("./app"); // Local modules
 
 console.log(sum(10, 10));
